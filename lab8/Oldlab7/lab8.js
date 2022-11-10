492593104e951a27976e97f89094ae21011b5c46
dangb24 <112642702+dangb24@users.noreply.github.com>
2022-11-10 10:22:19 -0500

    inclass php

---
+++ lab8/Oldlab7/lab8.js
@@ -0,0 +1,27 @@
+//this file reads in the projects json file and creates a dynamic menu of the labs
+
+$(document).ready(function() {
+  
+  	$.ajax({
+   	 	type: "GET",
+   	 	url: "projects.json",
+   	 	dataType: "json",
+   	 	success: function(responseData, status){
+   	  	var output = "<ul>";  
+    	 	$.each(responseData.items, function(i, item) {
+       		output += '<li>' + menuItem.title;
+			output += '<a href="' + menuItem.link + '" target="_blank">'; //link
+        	output += '<img src="' + menuItem.image + '" alt="' + menuItem.imTitle + '" /></a>'; //image
+			output += '<p>' + menuItem.description + '<p>';
+        	output += '</li>';
+      	});
+      	output += "</ul>";
+      	$('#projects').html(output);
+    	}, error: function(msg) {
+      				// there was a problem
+      	alert("There was a problem: " + msg.status + " " + msg.statusText);
+    	}
+  	});
+
+});
+
