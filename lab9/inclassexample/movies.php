8e9beed6abd1db433a4938ab3367b24de444010b
dangb24 <112642702+dangb24@users.noreply.github.com>
2022-11-21 18:32:56 -0500

    update movies.php and delete-movies.php

--- lab9/inclassexample/movies.php
+++ lab9/inclassexample/movies.php
@@ -93,7 +93,7 @@
   }
 ?>
 
-<h3>Add Actor</h3>
+<h3>Add Movie</h3>
 <form id="addForm" name="addForm" action="index.php" method="post" onsubmit="return validate(this);">
   <fieldset>
     <div class="formData">
@@ -110,7 +110,7 @@
 </form>
 
 <h3>Movies</h3>
-<table id="movieTable">
+<table id="actorTable">
 <?php
   if ($dbOk) {
 
